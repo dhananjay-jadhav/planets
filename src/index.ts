9aa7b5c9a4144f4211581a14c20de79106d71c76
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-09 15:18:32 +0530

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,62 @@
-import { parse } from 'csv-parse';
-import { createReadStream } from 'fs';
+import { parse } from "csv-parse";
+import { createReadStream } from "fs";
 
-const results: any = [];
+interface KeplerPlanet {
+  kepid: string;
+  kepoi_name: string;
+  kepler_name: string;
+  koi_disposition: string;
+  koi_pdisposition: string;
+  koi_score: string;
+  koi_fpflag_nt: string;
+  koi_fpflag_ss: string;
+  koi_fpflag_co: string;
+  koi_fpflag_ec: string;
+  koi_period: string;
+  koi_time0bk: string;
+  koi_impact: string;
+  koi_duration: string;
+  koi_depth: string;
+  koi_prad: string;
+  koi_teq: string;
+  koi_insol: string;
+  koi_model_snr: string;
+  koi_tce_plnt_num: string;
+  koi_steff: string;
+  koi_slogg: string;
+  koi_srad: string;
+  ra: string;
+  dec: string;
+  koi_kepmag: string;
+}
 
-createReadStream('data/kepler_planets.csv')
-    .pipe(parse({
-        comment: '#',
-        columns: true
-    }))
-    .on('data', (data) => {
-        results.push(data);
+const results: KeplerPlanet[] = [];
+
+function isHabitablePlanet(planet: KeplerPlanet) {
+  return (
+    planet.koi_disposition.toLowerCase() === "confirmed" &&
+    +planet.koi_insol > 0.36 &&
+    +planet.koi_insol < 1.11 &&
+    +planet.koi_prad < 1.6
+  );
+}
+
+createReadStream("data/kepler_planets.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
     })
-    .on('error', (err) => {
-        console.error(err);
-    })
-    .on('end', () => {
-        console.log(results);
-        console.log('File read done');
-    });
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.error(err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("Number of plantes like earth", results.length);
+  });
