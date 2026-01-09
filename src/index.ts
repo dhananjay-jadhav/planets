2363abba6ed59941fd44070fe69a58e9688cbfa6
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-09 15:02:44 +0530

    Fix it

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import parse from 'csv-parser';
+import { parse } from 'csv-parse';
 import { createReadStream } from 'fs';
 
 const results: any = [];
 
 createReadStream('data/kepler_planets.csv')
-    .pipe(parse())
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
     .on('data', (data) => {
         results.push(data);
     })
